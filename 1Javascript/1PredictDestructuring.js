804781852bb5e6169354ea4335968ca50e58a2cf
pmar0 <pmarino1994@gmail.com>
2021-09-29 18:35:43 -0400

    commit PredictDestructuring

---
+++ 1Javascript/1PredictDestructuring.js
@@ -0,0 +1,73 @@
+//Predict the output of the following:
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+/*Prediction:
+Tesla
+Mercedes
+*/
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+/*Prediction:
+error name not defined
+Elon
+*/
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+/*Prediction:
+12345
+undefined
+*/
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+/*Prediction:
+false
+true
+*/
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+/*Prediction:
+value
+[1,5,1,8,3,3]
+1
+5
+*/
+
