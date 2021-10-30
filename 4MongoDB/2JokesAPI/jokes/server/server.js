9962107f127a429be4ca3054900b841bbd04e488
pmar0 <pmarino1994@gmail.com>
2021-10-30 16:42:33 -0400

    jokesAPI modified server.js

--- 4MongoDB/2JokesAPI/jokes/server/server.js
+++ 4MongoDB/2JokesAPI/jokes/server/server.js
@@ -3,10 +3,8 @@ const app = express();
 const port = 8000;
 
 require("./config/mongoose.config");
+require("./routes/jokes.routes")(app);
 
 app.use(express.json(), express.urlencoded({ extended: true }));
-    
-const jokeRoutes = require("./routes/jokes.routes");
-jokeRoutes(app);
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
