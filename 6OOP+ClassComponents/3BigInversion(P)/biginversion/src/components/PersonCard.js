11035413b1c541c6f7adbd374df1db85a2f7696a
pmar0 <pmarino1994@gmail.com>
2021-11-18 14:35:52 -0500

    BigInversion first commit

---
+++ 6OOP+ClassComponents/3BigInversion(P)/biginversion/src/components/PersonCard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class PersonCard extends Component {
+  render(){
+    const {firstName,lastName,age,hairColor} = this.props
+
+    return(
+      <div className="Person">
+        <h2>{lastName}, {firstName}</h2>
+        <p>Age: {age}</p>
+        <p>Hair Color: {hairColor}</p>
+      </div>
+    );
+  }
+}
+
+export default PersonCard;
